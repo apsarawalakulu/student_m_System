d9349958de84c5df75ab6fd7c7ff23a92ad3315e
Apsara Walakulu <apsarawalakulu@gmail.com>
2025-06-06 17:04:52 +0530

    init commit

---
+++ Javaproject/CabBooking/src/FirstProgram/Mainclass.java
@@ -0,0 +1,9 @@
+package FirstProgram;
+
+public class Mainclass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Docter Appoinment System");
+    }
+
+}
---
+++ Javaproject/Javaproject/src/Javaproject/Main.java
@@ -0,0 +1,7 @@
+package Javaproject;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Doctor Appoiment System");
+    }
+}
